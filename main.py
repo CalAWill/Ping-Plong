a8c21f99d1092048107616ea7a6e752d64348146
Cal <79941980+CalAWill@users.noreply.github.com>
2021-03-08 16:56:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame
-pygame.init()
-import random
+# import random
 import time
 
-def ping():
+pygame.init()  # Initialises pygame
 
+def ping():
     # Player code
     playerImg = pygame.image.load("pingLeft96.png")
     playerX = 0
@@ -16,7 +16,7 @@ def ping():
     oppX = 800
     oppY = 202
     oppYMove = 0
-    oppXMove = 0
+    # oppXMove = 0
 
     # Ball
     ballImg = pygame.image.load("ball32.png")
@@ -24,8 +24,8 @@ def ping():
     ballY = 234
     ballXMove = -0.1
     ballYMove = 0.1
-    ballBeginY = random.randint(0, 1)
-    ballBeginX = random.randint(0, 1)
+    # ballBeginY = random.randint(0, 1)
+    # ballBeginX = random.randint(0, 1)
     ballBuildUp = 1.5
 
     # Text code
@@ -126,35 +126,63 @@ def ping():
         player(playerX, playerY)
         pygame.display.update()
 
+
 # Make screen
-screen = pygame.display.set_mode((900, 500))
-pygame.display.set_caption("Ping")
-pygame.display.set_icon(pygame.image.load("pingIcon96.png"))
-screen = pygame.display.set_mode((900, 500))
+screen = pygame.display.set_mode((900, 500))  # Sets the size of the window
+pygame.display.set_caption("Ping")  # Changes the window heading
+pygame.display.set_icon(pygame.image.load("pingIcon96.png"))  # Gives the window a specific image for the icon
 
 # Launch game
 ping()
 
-# # Replay text code
-# replayFont = pygame.font.Font("freesansbold.ttf", 64)
-# replayText = replayFont.render("REPLAY?", True, (255, 255, 255))
-#
-# running = True
-# while running:
-#     mousePos = pygame.mouse.get_pos()
-#     screen.fill((0, 0, 0))
-#     # Puts the boxes and text on screen
-#     screen.blit(replayText, (200, 250))
-#
-#     # If the mouse is over the yes box
-#     if 150 + 100 > mousePos[0] > 150 and 325 + 50 > mousePos[1] > 450:
-#         pygame.draw.rect(screen, (115, 255, 152), (225, 325, 100, 50))
-#     else:
-#         pygame.draw.rect(screen, (255, 255, 255), (225, 325, 100, 50))
-#
-#     if 150 + 100 > mousePos[0] > 150 and 325 + 50 > mousePos[1] > 450:
-#         pygame.draw.rect(screen, (255, 97, 102), (375, 325, 100, 50))
-#     else:
-#         pygame.draw.rect(screen, (255, 255, 255), (375, 325, 100, 50))
-#
-#     pygame.display.update()
+# Replay text code
+replayFont = pygame.font.Font("freesansbold.ttf", 64)
+replayText = replayFont.render("REPLAY?", True, (255, 255, 255))
+
+# Button text
+yesNoFont = pygame.font.Font("freesansbold.ttf", 32)
+yesText = yesNoFont.render("YES", True, (255, 255, 255))
+noText = yesNoFont.render("NO", True, (255, 255, 255))
+
+running = True
+while running:
+    # Gets the mouses x and y position
+    mousePos = pygame.mouse.get_pos()
+
+    # Refreshes the screen to black so updates can be made
+    screen.fill((0, 0, 0))
+
+    # Puts the boxes and text on screen
+    screen.blit(replayText, (200, 250))
+
+    # Looks for events
+    for event in pygame.event.get():
+        # If the player presses the exit window button, then quit the program
+        if event.type == pygame.QUIT:
+            running = False
+        # If the player clicks, check if they've clicked over one of the buttons
+        if event.type == pygame.MOUSEBUTTONUP:
+            # Have they clicked over the "yes" button? If so, call the ping() method
+            if 225 < mousePos[0] < 325 and 325 < mousePos[1] < 375:
+                ping()
+            # Have they clicked over the "no" button? If so, exit the loop and program as a result
+            if 375 < mousePos[0] < 475 and 325 < mousePos[1] < 375:
+                running = False
+
+    # If the mouse is over the yes box or no box, change the colour
+    if 225 < mousePos[0] < 325 and 325 < mousePos[1] < 375:
+        pygame.draw.rect(screen, (115, 255, 152), (225, 325, 100, 50))
+    else:
+        pygame.draw.rect(screen, (255, 255, 255), (225, 325, 100, 50))
+
+    if 375 < mousePos[0] < 475 and 325 < mousePos[1] < 375:
+        pygame.draw.rect(screen, (255, 97, 102), (375, 325, 100, 50))
+    else:
+        pygame.draw.rect(screen, (255, 255, 255), (375, 325, 100, 50))
+
+    # Adds the "yes" and "no" text to the buttons
+    screen.blit(yesText, (225, 325))
+    screen.blit(noText, (375, 325))
+
+    # Updates the screen on every loop
+    pygame.display.update()
